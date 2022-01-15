258674f95ff2df210166ae71408994873986defb
FaustVX <jodu035@gmail.com>
2022-01-15 20:04:38 +0100

    Sanitize dictionary input

--- Wordle.Core/Game.cs
+++ Wordle.Core/Game.cs
@@ -3,24 +3,44 @@
 namespace Wordle.Core;
 public class Game
 {
-    public static readonly Dictionary<int, string[]> _words;
+    public static readonly Dictionary<int, string[]> Words;
 
     static Game()
     {
         using var http = new HttpClient();
-        _words = http.GetStringAsync(@"https://raw.githubusercontent.com/hbenbel/French-Dictionary/master/dictionary/dictionary.txt")
+        Words = http.GetStringAsync(@"https://raw.githubusercontent.com/hbenbel/French-Dictionary/master/dictionary/dictionary.txt")
             .GetAwaiter()
             .GetResult()
             .Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
             .Where([DebuggerStepThroughAttribute] static (word) => word.Length >= 3)
+            .Where([DebuggerStepThroughAttribute] static (word) => !word.Contains('-'))
+            .Select([DebuggerStepThroughAttribute] static (word) => Sanitize(word))
             .GroupBy([DebuggerStepThroughAttribute] static (word) => word.Length)
             .Select([DebuggerStepThroughAttribute] static (group) => (key: group.Key, words: group.ToArray()))
             .Where([DebuggerStepThroughAttribute] static (group) => group.words.Length > 10)
             .OrderBy([DebuggerStepThroughAttribute] static (group) => group.key)
             .ToDictionary([DebuggerStepThroughAttribute] static (group) => group.key, [DebuggerStepThroughAttribute] static (group) => group.words);
+
+        static string Sanitize(string word)
+            => word.ToLower()
+                .Replace('â', 'a')
+                .Replace('à', 'a')
+                .Replace('ä', 'a')
+                .Replace('é', 'e')
+                .Replace('è', 'e')
+                .Replace('ê', 'e')
+                .Replace('ë', 'e')
+                .Replace('ï', 'i')
+                .Replace('î', 'i')
+                .Replace('ô', 'o')
+                .Replace('ö', 'o')
+                .Replace('û', 'u')
+                .Replace('ü', 'u')
+                .Replace('ù', 'u')
+                .Replace('ç', 'c');
     }
 
-    public static IEnumerable<int> ValideWordLength => _words.Keys;
+    public static IEnumerable<int> ValideWordLength => Words.Keys;
 
     public int WordLength { get; }
     public int PossibleTries { get; }
@@ -28,11 +48,11 @@ public class Game
     public int RemainingTries { get; private set; }
 
     public Game(int wordLength, int possibleTries)
-        => (WordLength, PossibleTries, RemainingTries, SelectedWord) = (wordLength, possibleTries, possibleTries, _words[wordLength][new Random().Next(_words[wordLength].Length)]);
+        => (WordLength, PossibleTries, RemainingTries, SelectedWord) = (wordLength, possibleTries, possibleTries, Words[wordLength][new Random().Next(Words[wordLength].Length)]);
 
     public Letter[]? Try(string word)
     {
-        if (RemainingTries <= 0 || word.Length != SelectedWord.Length || !_words[WordLength].Contains(word))
+        if (RemainingTries <= 0 || word.Length != SelectedWord.Length || !Words[WordLength].Contains(word))
             return null;
 
         RemainingTries--;
