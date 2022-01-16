e8d6e007a6484a86d2eef78cbd8b4b46237ba54d
FaustVX <jodu035@gmail.com>
2022-01-16 15:08:17 +0100

    Added a replay loop

--- Wordle.App/Program.cs
+++ Wordle.App/Program.cs
@@ -3,55 +3,59 @@ using static ConsoleMenu.Helpers;
 using System.Diagnostics;
 using static Wordle.App.Options;
 
-var game = Start();
-var placedLetters = new (char? wellPlaced, HashSet<char>? invalid)[game.WordLength];
-var validLetters = new HashSet<char>();
-var invalidLetters = new HashSet<char>();
-
-Console.Clear();
-WriteHeader(game, placedLetters, validLetters, invalidLetters);
-
-do
+for (var game = Start(); true; game = new(game.WordLength, game.PossibleTries))
 {
-    Console.Write($"{game.RemainingTries - 1}: ");
-    var word = game.Try(Input(game, placedLetters, validLetters, invalidLetters));
-    if (word is null)
-    {
-        Console.WriteLine("Invalid word!");
-        continue;
-    }
-    Console.CursorTop--;
-    Console.Write($"{game.RemainingTries}> ");
-    for (int i = 0; i < word.Length; i++)
-    {
-        var letter = word[i];
-        switch (letter)
-        {
-            case { IsWellPlaced: true }:
-                Write(letter.Char, WellPlacedColor);
-                placedLetters[i].wellPlaced = letter.Char;
-                validLetters.Add(letter.Char);
-                break;
-            case { IsValid: true }:
-                Write(letter.Char, ValidColor);
-                validLetters.Add(letter.Char);
-                (placedLetters[i].invalid ??= new()).Add(letter.Char);
-                break;
-            default:
-                Write(letter.Char, InvalidColor);
-                invalidLetters.Add(letter.Char);
-                break;
-        }
-    }
-    Console.WriteLine();
-    if (word.All([DebuggerStepThrough] static (letter) => letter.IsWellPlaced))
-    {
-        Console.WriteLine("Well played");
-        break;
-    }
-} while (game.RemainingTries > 0);
+    var placedLetters = new (char? wellPlaced, HashSet<char>? invalid)[game.WordLength];
+    var validLetters = new HashSet<char>();
+    var invalidLetters = new HashSet<char>();
 
-Console.WriteLine($"The word was {game.SelectedWord}");
+    Console.Clear();
+    WriteHeader(game, placedLetters, validLetters, invalidLetters);
+
+    do
+    {
+        Console.Write($"{game.RemainingTries - 1}: ");
+        var word = game.Try(Input(game, placedLetters, validLetters, invalidLetters));
+        if (word is null)
+        {
+            Console.WriteLine("Invalid word!");
+            continue;
+        }
+        Console.CursorTop--;
+        Console.Write($"{game.RemainingTries}> ");
+        for (int i = 0; i < word.Length; i++)
+        {
+            var letter = word[i];
+            switch (letter)
+            {
+                case { IsWellPlaced: true }:
+                    Write(letter.Char, WellPlacedColor);
+                    placedLetters[i].wellPlaced = letter.Char;
+                    validLetters.Add(letter.Char);
+                    break;
+                case { IsValid: true }:
+                    Write(letter.Char, ValidColor);
+                    validLetters.Add(letter.Char);
+                    (placedLetters[i].invalid ??= new()).Add(letter.Char);
+                    break;
+                default:
+                    Write(letter.Char, InvalidColor);
+                    invalidLetters.Add(letter.Char);
+                    break;
+            }
+        }
+        Console.WriteLine();
+        if (word.All([DebuggerStepThrough] static (letter) => letter.IsWellPlaced))
+        {
+            Console.WriteLine("Well played");
+            break;
+        }
+    } while (game.RemainingTries > 0);
+
+    Console.WriteLine($"The word was {game.SelectedWord}");
+    Console.WriteLine("Press any key to restart a game");
+    Console.ReadLine();
+}
 
 static void WriteHeader(Game game, (char? wellPlaced, HashSet<char>? invalid)[] placedLetters, IReadOnlyCollection<char> validLetters, IReadOnlyCollection<char> invalidLetters)
 {
@@ -71,7 +75,7 @@ static Game Start()
 
     var length = Menu("Select word length", Game.ValideWordLength.ToArray(), [DebuggerStepThrough] static (i) => i.ToString());
     var tries = Menu("Select possible tries", Enumerable.Range(4, 7).ToArray(), [DebuggerStepThrough] static (i) => i.ToString());
-    return new Game(length, tries);
+    return new(length, tries);
 }
 
 static string Input(Game game, (char? wellPlaced, HashSet<char>? invalid)[] placedLetters, IReadOnlyCollection<char> validLetters, IReadOnlyCollection<char> invalidLetters)
